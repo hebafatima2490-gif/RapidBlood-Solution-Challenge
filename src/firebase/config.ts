17199308173fc369d43928ae530c4e519bbe831a
Heba Fatima <hebafatima2490@gmail.com>
2026-04-28 16:04:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7481561832-42137",
+  "appId": "1:432376818880:web:550d9ba5ef3935e8d4d125",
+  "apiKey": "AIzaSyDk8sOP4kqKkf1hmzt6sgR34ks3VDPyw54",
+  "authDomain": "studio-7481561832-42137.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "432376818880"
+};
